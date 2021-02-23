6ae5a7aeb896f55a26eed7c8c61eafc34dee4455
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-02-23 18:16:10 -0300

    Configure error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import 'dotenv/config';
+import 'express-async-errors';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import { isBoom } from '@hapi/boom';
+
 import './database';
 import routes from './routes';
 
@@ -11,4 +15,21 @@ app.use(helmet());
 app.use(express.json());
 
 app.use('/v1', routes);
+
+app.use(
+  async (err: Error, request: Request, res: Response, next: NextFunction) => {
+    if (isBoom(err)) {
+      const { statusCode, payload } = err.output;
+
+      return res.status(statusCode).json({
+        ...payload,
+        ...err.data,
+        docs: process.env.DOCS_URL,
+      });
+    }
+
+    return next(err);
+  }
+);
+
 export default app;
