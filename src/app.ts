2f21cd323374c4b22c84ab3d99f660b7bd3d83a3
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-02-23 18:15:25 -0300

    Import routes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import cors from 'cors';
 import helmet from 'helmet';
+import routes from './routes';
+
 const app = express();
 
 app.use(cors());
 app.use(helmet());
+app.use(express.json());
+
+app.use('/v1', routes);
 export default app;
