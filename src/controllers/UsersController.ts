5f4dbb6a2a6193ef730f6b8fc5a26ce3867e9aef
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-02-24 18:13:33 -0300

    Fix controller name and update logic

---
+++ src/controllers/UsersController.ts
@@ -0,0 +1,26 @@
+import { badRequest } from '@hapi/boom';
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+
+import UsersRepository from '../repositories/UsersRepository';
+
+class UsersController {
+  async create(request: Request, response: Response): Promise<Response> {
+    const { name, email } = request.body;
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const userAlreadyExists = await usersRepository.findOne({
+      email,
+    });
+    if (userAlreadyExists) {
+      throw badRequest('User already exists!', { code: 140 });
+    }
+
+    const user = usersRepository.create({ name, email });
+    await usersRepository.save(user);
+
+    return response.status(201).json(user);
+  }
+}
+
+export default UsersController;
