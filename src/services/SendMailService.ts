9ebd5d22c381f3faef1d5f7a457619d93177617a
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-02-25 18:02:56 -0300

    Create service to send email

---
+++ src/services/SendMailService.ts
@@ -0,0 +1,42 @@
+import nodemailer, { Transporter } from 'nodemailer';
+import handlebars from 'handlebars';
+import fs from 'fs';
+
+class SendMailService {
+  private client: Transporter;
+
+  constructor() {
+    nodemailer.createTestAccount().then((account) => {
+      const transporter = nodemailer.createTransport({
+        host: account.smtp.host,
+        port: account.smtp.port,
+        secure: account.smtp.secure,
+        auth: {
+          user: account.user,
+          pass: account.pass,
+        },
+      });
+
+      this.client = transporter;
+    });
+  }
+
+  async execute(to: string, subject: string, variables: objerct, path: string) {
+    const template = fs.readFileSync(path).toString('utf8');
+
+    const templateBuilder = handlebars.compile(template);
+    const html = templateBuilder(variables);
+
+    const message = await this.client.sendMail({
+      to,
+      subject,
+      html,
+      from: 'NPS <noreply@npser.com>',
+    });
+
+    console.log('Message sent %s', message.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
+  }
+}
+
+export default SendMailService;
