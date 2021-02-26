0c3b0787b4067c37db7afdd0b0eadf88b0e34daf
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-02-26 20:18:48 -0300

    Create answer controller

---
+++ src/controllers/AnswersController.ts
@@ -0,0 +1,34 @@
+import { notFound } from '@hapi/boom';
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+
+import SurveysUsersRepository from '../repositories/SurveysUsersRepository';
+
+class AnswersController {
+  async index(request: Request, response: Response): Promise<Response> {
+    const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+    const surveysUsers = await surveysUsersRepository.find({
+      relations: ['user', 'survey'],
+    });
+
+    return response.json(surveysUsers);
+  }
+
+  async store(request: Request, response: Response): Promise<Response> {
+    const { value } = request.params;
+    const { id } = request.query;
+
+    const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+    const surveyUser = await surveysUsersRepository.findOne({ id: String(id) });
+    if (!surveyUser) {
+      throw notFound('SurveyUser not found');
+    }
+
+    surveyUser.value = Number(value);
+    await surveysUsersRepository.save(surveyUser);
+
+    return response.sendStatus(204);
+  }
+}
+
+export default AnswersController;
