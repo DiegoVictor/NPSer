b70e39ac5aca7339d7f7124138eb278587adc33a
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-02-26 20:20:32 -0300

    Configure answer routes

--- src/routes.ts
+++ src/routes.ts
@@ -3,17 +3,20 @@ import { Router } from 'express';
 import SurveysController from './controllers/SurveysController';
 import UsersController from './controllers/UsersController';
 import SendMailController from './controllers/SendMailController';
+import AnswersController from './controllers/AnswersController';
 import NpsController from './controllers/NpsController';
 import userValidator from './validators/userValidator';
 import surveyValidator from './validators/surveyValidator';
 import sendEmailValidator from './validators/sendEmailValidator';
 import npsValidator from './validators/npsValidator';
+import answerValidator from './validators/answerValidator';
 
 const routes = Router();
 
 const usersController = new UsersController();
 const surveysController = new SurveysController();
 const sendMailController = new SendMailController();
+const answersController = new AnswersController();
 const npsController = new NpsController();
 
 routes.post('/users', userValidator, usersController.store);
@@ -22,6 +25,9 @@ routes.get('/surveys', surveysController.index);
 routes.post('/surveys', surveyValidator, surveysController.store);
 
 routes.post('/send_mail', sendEmailValidator, sendMailController.store);
+
+routes.get('/answers', answersController.index);
+routes.get('/answers/:value', answerValidator, answersController.store);
 
 routes.get('/nps/:survey_id', npsValidator, npsController.show);
 
