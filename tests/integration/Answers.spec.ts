8247b964ce157d6e950050b730c5dfe6c501998d
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-02-27 01:14:01 -0300

    Test answers endpoints

---
+++ tests/integration/Answers.spec.ts
@@ -0,0 +1,170 @@
+import request from 'supertest';
+import faker from 'faker';
+
+import app from '../../src/app';
+import factory from '../utils/factory';
+import createConnection from '../../src/database/index';
+import { getRepository } from 'typeorm';
+import Survey from '../../src/models/Survey';
+import SurveyUser from '../../src/models/SurveyUser';
+import User from '../../src/models/User';
+
+interface UserType {
+  id: string;
+  name: string;
+  email: string;
+  created_at: Date;
+}
+
+interface SurveyType {
+  id: string;
+  title: string;
+  description: string;
+  created_at: Date;
+}
+
+interface SurveyUserType {
+  id: string;
+  user_id: string;
+  survey_id: string;
+  value: number;
+  created_at: Date;
+}
+
+describe('Users', () => {
+  beforeAll(async () => {
+    const connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  beforeEach(async () => {
+    const surveysUsersRepository = getRepository(SurveyUser);
+    await surveysUsersRepository.clear();
+
+    const usersRepository = getRepository(User);
+    await usersRepository.clear();
+
+    const surveysRepository = getRepository(Survey);
+    await surveysRepository.clear();
+  });
+
+  afterAll(async () => {
+    const connection = await createConnection();
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it('should be able to answer a survey', async () => {
+    const usersRepository = getRepository(User);
+    const surveysUsersRepository = getRepository(SurveyUser);
+    const surveysRepository = getRepository(Survey);
+
+    const users = await factory.attrsMany<UserType>('User', 3);
+    const promises: Promise<{
+      user: UserType;
+      survey: SurveyType;
+      surveyUser: SurveyUserType;
+    }>[] = users.map((user) => {
+      return new Promise((resolve) => {
+        const savedUser = usersRepository.create(user);
+
+        usersRepository.save(savedUser).then(() => {
+          factory.attrs<SurveyType>('Survey').then((survey) => {
+            const savedSurvey = surveysRepository.create(survey);
+
+            surveysRepository.save(savedSurvey).then(() => {
+              factory
+                .attrs<SurveyUserType>('SurveyUser', {
+                  user_id: savedUser.id,
+                  survey_id: savedSurvey.id,
+                })
+                .then((surveyUser) => {
+                  const savedSurveyUser = surveysUsersRepository.create(
+                    surveyUser
+                  );
+
+                  surveysUsersRepository.save(savedSurveyUser).then(() => {
+                    resolve({
+                      user: savedUser,
+                      survey: savedSurvey,
+                      surveyUser: savedSurveyUser,
+                    });
+                  });
+                });
+            });
+          });
+        });
+      });
+    });
+    const surveysUsers = await Promise.all(promises);
+
+    const response = await request(app).get(`/v1/answers`).expect(200);
+
+    surveysUsers.forEach(({ survey, surveyUser, user }) => {
+      expect(response.body).toContainEqual({
+        ...surveyUser,
+        created_at: surveyUser.created_at.toISOString(),
+        user: {
+          ...user,
+          created_at: user.created_at.toISOString(),
+        },
+        survey: {
+          ...survey,
+          created_at: survey.created_at.toISOString(),
+        },
+      });
+    });
+  });
+
+  it('should be able to answer a survey', async () => {
+    const user = await factory.attrs<UserType>('User');
+    const survey = await factory.attrs<SurveyType>('Survey');
+
+    const usersRepository = getRepository(User);
+    const savedUser = usersRepository.create(user);
+    await usersRepository.save(savedUser);
+
+    const surveysRepository = getRepository(Survey);
+    const savedSurvey = surveysRepository.create(survey);
+    await surveysRepository.save(savedSurvey);
+
+    const surveyUser = await factory.attrs<SurveyUserType>('SurveyUser', {
+      user_id: savedUser.id,
+      survey_id: savedSurvey.id,
+      value: null,
+    });
+    const surveysUsersRepository = getRepository(SurveyUser);
+    const savedSurveyUser = surveysUsersRepository.create(surveyUser);
+    await surveysUsersRepository.save(savedSurveyUser);
+
+    const value = faker.random.number({ min: 1, max: 10 });
+    await request(app)
+      .get(`/v1/answers/${value}?id=${savedSurveyUser.id}`)
+      .expect(204)
+      .send();
+
+    const updatedSurveyUser = await surveysUsersRepository.findOne(
+      savedSurveyUser.id
+    );
+
+    expect(updatedSurveyUser.value).toBe(value);
+  });
+
+  it('should not be able to answer a survey that not exists', async () => {
+    const value = faker.random.number({ min: 1, max: 10 });
+    const id = faker.random.uuid();
+
+    const response = await request(app)
+      .get(`/v1/answers/${value}?id=${id}`)
+      .expect(404)
+      .send();
+
+    expect(response.body).toStrictEqual({
+      statusCode: 404,
+      code: 242,
+      error: 'Not Found',
+      message: 'SurveyUser not found',
+      docs: process.env.DOCS_URL,
+    });
+  });
+});
