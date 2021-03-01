579b9a85acf4e4f98a4673e4ad5c77bb7476b5c2
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-02-28 21:18:00 -0300

    Test second page of surveys

--- tests/integration/Surveys.spec.ts
+++ tests/integration/Surveys.spec.ts
@@ -51,6 +51,37 @@ describe('Surveys', () => {
     });
   });
 
+  it('should be able the second page of surveys', async () => {
+    const surveys = await factory.attrsMany<SurveyType>('Survey', 20);
+
+    const surveysRepository = getRepository(Survey);
+
+    const createdSurveys = surveysRepository.create(surveys);
+    await surveysRepository.save(createdSurveys);
+
+    const response = await request(app).get('/v1/surveys?page=2');
+
+    expect(response.body.length).toBe(10);
+    createdSurveys
+      .sort((a, b) => {
+        if (a.id > b.id) {
+          return 1;
+        } else if (b.id > a.id) {
+          return -1;
+        }
+        return 0;
+      })
+      .slice(-10)
+      .forEach((survey) => {
+        expect(response.body).toContainEqual({
+          id: survey.id,
+          title: survey.title,
+          description: survey.description,
+          created_at: survey.created_at.toISOString(),
+        });
+      });
+  });
+
   it('should be able to create a new survey', async () => {
     const survey = await factory.attrs<SurveyType>('Survey');
 
