4c7fb420efea06f3ba091fc69e61e7052cb86932
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-02-28 23:38:42 -0300

    Paginate results

--- src/controllers/AnswersController.ts
+++ src/controllers/AnswersController.ts
@@ -6,9 +6,14 @@ import SurveysUsersRepository from '../repositories/SurveysUsersRepository';
 
 class AnswersController {
   async index(request: Request, response: Response): Promise<Response> {
+    const { page = 1 } = request.query;
+    const limit = 10;
+
     const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
     const surveysUsers = await surveysUsersRepository.find({
       relations: ['user', 'survey'],
+      take: limit,
+      skip: (Number(page) - 1) * limit,
     });
 
     const count = await surveysUsersRepository.count();
--- src/controllers/SurveysController.ts
+++ src/controllers/SurveysController.ts
@@ -5,8 +5,14 @@ import SurveysRepository from '../repositories/SurveysRepository';
 
 class SurveysController {
   async index(request: Request, response: Response): Promise<Response> {
+    const { page = 1 } = request.query;
+    const limit = 10;
+
     const surveysRepository = getCustomRepository(SurveysRepository);
-    const surveys = await surveysRepository.find();
+    const surveys = await surveysRepository.find({
+      take: limit,
+      skip: (Number(page) - 1) * limit,
+    });
 
     const count = await surveysRepository.count();
     response.header('X-Total-Count', count.toString());
