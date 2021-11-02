acbe417350ac886365754b4d76a074bbec806f9e
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-11-02 17:30:25 -0300

    Set page validator in routes

--- src/routes.ts
+++ src/routes.ts
@@ -10,6 +10,7 @@ import surveyValidator from './validators/surveyValidator';
 import sendEmailValidator from './validators/sendEmailValidator';
 import npsValidator from './validators/npsValidator';
 import answerValidator from './validators/answerValidator';
+import pageValidator from './validators/pageValidator';
 
 const app = Router();
 
@@ -21,12 +22,12 @@ const npsController = new NpsController();
 
 app.post('/users', userValidator, usersController.store);
 
-app.get('/surveys', surveysController.index);
+app.get('/surveys', pageValidator, surveysController.index);
 app.post('/surveys', surveyValidator, surveysController.store);
 
 app.post('/send_mail', sendEmailValidator, sendMailController.store);
 
-app.get('/answers', answersController.index);
+app.get('/answers', pageValidator, answersController.index);
 app.get('/answers/:value', answerValidator, answersController.store);
 
 app.get('/nps/:survey_id', npsValidator, npsController.show);
