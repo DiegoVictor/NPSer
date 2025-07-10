2ed3678fce7dafe73d2c7ab7e5b61fb858c7daca
DiegoVictor <diegovictorgonzaga@gmail.com>
2025-07-10 15:28:47 -0300

    Update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -8,7 +8,7 @@ export default {
   // automock: false,
 
   // Stop running tests after `n` failures
-  bail: true,
+  // bail: 0,
 
   // The directory where Jest should store its cached dependency information
   // cacheDirectory: "C:\\Users\\Diego\\AppData\\Local\\Temp\\jest",
